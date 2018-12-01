9c136e7eb650a71d2796f5e94199cf46b535a1b4
Daniel Lublin <daniel@lublin.se>
2018-12-01 19:23:08 +0100

    Add day 1, part 2

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ use std::fs::File;
 use std::io;
 use std::io::{BufRead, BufReader, Error, ErrorKind};
 
+use std::collections::HashSet;
+use std::process;
 
 fn read_ints(path: &str) -> Result<Vec<i32>, io::Error> {
     let f = File::open(path)?;
@@ -23,9 +25,23 @@ fn main() {
     let mut freq: i32 = 0;
 
     let changes = read_ints("input").expect("ints");
+    let mut seen = HashSet::new();
 
-    for i in changes.clone() {
+    for i in changes.iter() {
         freq += i;
+        seen.insert(freq);
     }
     println!("part1 freq: {:?}", freq);
+
+    loop {
+        for i in changes.iter() {
+            freq += i;
+            if seen.contains(&freq) {
+                println!("part2 freq: {:?}", freq);
+                process::exit(0);
+            } else {
+                seen.insert(freq);
+            }
+        }
+    }
 }
