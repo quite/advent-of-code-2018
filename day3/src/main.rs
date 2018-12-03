db0ce13c0550045aa07652063150d2ac7da30dbf
Daniel Lublin <daniel@lublin.se>
2018-12-03 16:44:47 +0100

    Solve day3, part1

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -3,6 +3,8 @@ use std::io;
 use std::io::{BufRead, BufReader};
 
 use std::collections::HashMap;
+#[macro_use]
+extern crate itertools;
 
 fn read_lines(path: &str) -> Result<Vec<String>, io::Error> {
     let f = File::open(path)?;
@@ -23,23 +25,21 @@ fn line_to_rect(line: &str) -> Option<Rectangle> {
         .map(|s| s.parse().unwrap())
         .collect();
     match parsed.as_slice() {
-        [id, x, y, w, h] => Some(Rectangle {
-            id: *id,
+        [_id, x, y, w, h] => Some(Rectangle {
             x: *x,
             y: *y,
-            x2: *x + *w,
-            y2: *y + *h,
+            w: *w,
+            h: *h,
         }),
         _ => None,
     }
 }
 
 struct Rectangle {
-    id: i32,
     x: i32,
     y: i32,
-    x2: i32,
-    y2: i32,
+    w: i32,
+    h: i32,
 }
 
 fn main() {
@@ -51,7 +51,19 @@ fn main() {
         rects.push(line_to_rect(l).unwrap());
     }
 
+    let mut overlaps = HashMap::new();
     for r in rects.iter() {
-        println!("{:?} {:?} {:?} {:?} {:?}", r.id, r.x, r.y, r.x2, r.y2);
+        for patch in iproduct!(r.x..r.x + r.w, r.y..r.y + r.h) {
+            let entry = overlaps.entry(patch).or_insert(0);
+            *entry += 1;
+        }
     }
+
+    println!(
+        "part1 overlapping claims: {:?}",
+        overlaps
+            .iter()
+            .filter(|&(_patch, count)| *count >= 2)
+            .count()
+    );
 }
