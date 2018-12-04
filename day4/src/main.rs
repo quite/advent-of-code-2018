a9fefa1cb3299097d97bfc123ff496d00400d5fc
Daniel Lublin <daniel@lublin.se>
2018-12-04 23:36:48 +0100

    Solve day4, part1

---
+++ day4/src/main.rs
@@ -0,0 +1,104 @@
+extern crate tools;
+use tools::read_from_file;
+
+extern crate regex;
+use regex::Regex;
+use std::collections::HashMap;
+
+#[derive(Debug)]
+struct Event {
+    timestamp: Timestamp,
+    eventtype: EventType,
+}
+
+#[derive(Debug, Copy, Clone, Eq, PartialEq, Ord, PartialOrd)]
+struct Timestamp {
+    y: u32,
+    m: u32,
+    d: u32,
+    hour: u32,
+    min: u32,
+}
+
+#[derive(Debug)]
+enum EventType {
+    BeginShift { id: u32 },
+    FallAsleep,
+    WakeUp,
+}
+
+fn line_to_event(line: &str) -> Option<Event> {
+    let re = Regex::new(r"^\[(?P<y>\d{4})-(?P<m>\d{2})-(?P<d>\d{2})\s(?P<hour>\d{2}):(?P<min>\d{2})\]\s(?:Guard\s#(?P<id>\d+)\sbegins.*|(?P<change>.*))$",
+    ).unwrap();
+
+    let cap = re.captures(line).unwrap();
+
+    let timestamp = Timestamp {
+        y: cap.name("y").unwrap().as_str().parse().unwrap(),
+        m: cap.name("m").unwrap().as_str().parse().unwrap(),
+        d: cap.name("d").unwrap().as_str().parse().unwrap(),
+        hour: cap.name("hour").unwrap().as_str().parse().unwrap(),
+        min: cap.name("min").unwrap().as_str().parse().unwrap(),
+    };
+
+    let eventtype = if let Some(idstr) = cap.name("id") {
+        EventType::BeginShift {
+            id: idstr.as_str().parse().unwrap(),
+        }
+    } else if &cap["change"] == "falls asleep" {
+        EventType::FallAsleep
+    } else if &cap["change"] == "wakes up" {
+        EventType::WakeUp
+    } else {
+        return None;
+    };
+
+    Some(Event {
+        timestamp,
+        eventtype,
+    })
+}
+
+fn main() {
+    let input = read_from_file("input").unwrap();
+
+    let mut events: Vec<Event> = Vec::new();
+
+    for l in input.lines() {
+        events.push(line_to_event(l).unwrap());
+    }
+
+    // mapping guard id to minute to frequency
+    let mut guardminfreq: HashMap<u32, HashMap<u32, u32>> = HashMap::new();
+    events.sort_by(|a, b| a.timestamp.cmp(&b.timestamp));
+    let mut thisguard = 0;
+    let mut sleepingsince: Option<Timestamp> = None;
+    for e in events {
+        match e.eventtype {
+            EventType::BeginShift { id } => {
+                thisguard = id;
+                sleepingsince = None;
+            }
+            EventType::FallAsleep => {
+                sleepingsince = Some(e.timestamp);
+            }
+            EventType::WakeUp => {
+                let minfreq = &mut (*guardminfreq.entry(thisguard).or_default());
+                for min in sleepingsince.unwrap().min..e.timestamp.min {
+                    *minfreq.entry(min).or_default() += 1;
+                }
+            }
+        }
+    }
+
+    // pick out id of guard sleeping the most
+    let (id, minfreq) = guardminfreq
+        .iter()
+        .max_by_key(|&(_, minfreq)| minfreq.values().sum::<u32>())
+        .unwrap();
+
+    // find the minute that this guard most frequently sleeps on
+    let (min, _) = minfreq.iter().max_by_key(|&(_, freq)| freq).unwrap();
+
+    println!("part1, {:?} * {:?} = {:?}", id, min, id * min);
+}
