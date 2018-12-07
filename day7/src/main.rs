f0a6460ccd2597505f7da30ed1fe973a77bf6193
Daniel Lublin <daniel@lublin.se>
2018-12-07 11:39:04 +0100

    Use HashSet for reqs; simplify

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -2,6 +2,7 @@ extern crate tools;
 use tools::read_from_file;
 
 use std::collections::HashMap;
+use std::collections::HashSet;
 
 extern crate regex;
 use regex::Regex;
@@ -13,34 +14,36 @@ fn main() {
     let input = read_from_file("input").unwrap();
 
     // map step -> requirements
-    let mut stepreqs: HashMap<char, Vec<char>> = HashMap::new();
+    let mut stepreqs: HashMap<char, HashSet<char>> = HashMap::new();
 
     let re = Regex::new(r"^Step (?P<req>.) m.*p (?P<id>.) can begin\.$").unwrap();
     for l in input.lines() {
         let cap = re.captures(l).unwrap();
         let id: &char = &cap["id"].chars().next().unwrap();
         let req: &char = &cap["req"].chars().next().unwrap();
-        // store/extend this step's requirements
+        // add this step and the new requirement
         stepreqs
             .entry(id.clone())
-            .or_insert(vec![])
-            .extend(vec![req]);
-        // if this req is not already a step, store it without reqs
-        stepreqs.entry(req.clone()).or_insert(vec![]);
+            .or_insert(HashSet::new())
+            .insert(*req);
+        // if the req is not already a step, store it without reqs
+        stepreqs.entry(req.clone()).or_insert(HashSet::new());
     }
 
     // set of steps done, ordered
     let mut done: LinkedHashSet<char> = LinkedHashSet::new();
 
     while !stepreqs.is_empty() {
-        let mut todo: Vec<char> = stepreqs.keys().cloned().collect();
-        todo.sort();
-        for step in todo {
+        for c in b'A'..=b'Z' {
+            let step = c as char;
             if let Some(reqs) = stepreqs.get(&step).cloned() {
-                // are all reqs for this step done?
-                if reqs.iter().filter(|&r| !done.contains(r)).count() == 0 {
+                if reqs.is_empty() {
                     stepreqs.remove(&step);
                     done.insert(step);
+                    // remove the step from all steps' requirement sets
+                    for (_, r) in stepreqs.iter_mut() {
+                        r.retain(|&s| s != step);
+                    }
                     // restart in alphabetic order
                     break;
                 }
